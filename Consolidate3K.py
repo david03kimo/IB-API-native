b87e719b20cd5c0f55fc9e1a8b79ff08b831902e
David Liao <david03kimo@gmail.com>
2020-10-09 19:29:31 +0800

    better performance and accurate ultimated

--- Consolidate3K.py
+++ Consolidate3K.py
@@ -17,6 +17,8 @@ class TestApp(EWrapper,EClient):
         EClient.__init__(self,self)
         self.data = [] #Historical
         self.data1 = [] #Update
+        self.df=[] # Historical
+        self.df1=[] #Update
         self.period=3 #3m K
         # resample method dictionary
         self.res_dict = {
@@ -34,34 +36,37 @@ class TestApp(EWrapper,EClient):
 
     def historicalData(self,reqId,bar):
         self.data.append([bar.date, bar.open,bar.high,bar.low,bar.close,bar.volume])
-        df = pd.DataFrame(self.data,columns=['DateTime','Open','High','Low', 'Close','Volume'])
-        df['DateTime'] = pd.to_datetime(df['DateTime'],unit='s') 
-        df.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/3K_Historical.csv',index=0 ,float_format='%.5f')
-
-
+        self.now_date=int(bar.date)
+         
     def historicalDataEnd(self, reqId, start: str, end: str):
+        self.data1.append(self.data[-1])
+        del self.data[-1]
+        self.df = pd.DataFrame(self.data,columns=['DateTime','Open','High','Low', 'Close','Volume'])
+        self.df['DateTime'] = pd.to_datetime(self.df['DateTime'],unit='s')
+        self.df.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/3K.csv',index=0 ,float_format='%.5f')   
+        self.data=[] #清掉是否有助於記憶體的節省？
         super().historicalDataEnd(reqId, start, end)
         print('-------------------------------------------------------------------------------------------------------------')
         print("HistoricalDataEnd. ReqId:", reqId, "from", start, "to", end)
         print('-------------------------------------------------------------------------------------------------------------')
-
+        
     def historicalDataUpdate(self, reqId: int, bar):
         self.data1.append([bar.date,bar.open,bar.high,bar.low,bar.close,bar.volume])
-        df1 = pd.DataFrame(self.data1,columns=['DateTime','Open','High','Low', 'Close','Volume'])
-        df1['DateTime'] = pd.to_datetime(df1['DateTime'],unit='s') 
-        df1=df1.set_index('DateTime')
+        self.df1 = pd.DataFrame(self.data1,columns=['DateTime','Open','High','Low', 'Close','Volume'])
+        self.df1['DateTime'] = pd.to_datetime(self.df1['DateTime'],unit='s') 
+        self.df1=self.df1.set_index('DateTime')
      
         #Calculate the bar.date and previous bar.date
         self.pre_date=self.now_date
         self.now_date=int(bar.date)
         
-        if self.now_date%(self.period*60) ==0 and self.pre_date%(self.period*60)==120 : #Resample once after the bar closed
-            res_df=df1.resample('3min', closed='left', label='left').agg(self.res_dict)
-            print('Resampled')
+        if self.now_date != self.pre_date : #Resample once after the bar closed
+            res_df=self.df1.resample('3min', closed='left', label='left').agg(self.res_dict)
+            del self.data1[0:len(self.data1)-1]
             res_df.drop(res_df.index[-1], axis=0, inplace=True) #delete the new open bar at lastest appended row
-            res_df.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/res3K_HistoricalUpdate.csv',float_format='%.5f')
-
-        df1.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/3K_HistoricalUpdate.csv' ,float_format='%.5f')
+            print('Resampled',datetime.fromtimestamp(self.now_date-180))
+            res_df.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/3K.csv', mode='a', header=False,float_format='%.5f')
+        self.df1.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/3K_HistoricalUpdate.csv' ,float_format='%.5f')
         
     def nextValidId(self,orderId):
         self.nextOrderId=orderId
@@ -77,7 +82,6 @@ class TestApp(EWrapper,EClient):
         self.done=True
         self.disconnect()
 
-    
 def main():
     app=TestApp()
     app.nextOrderId=0
@@ -90,7 +94,7 @@ def main():
     contract.exchange = "IDEALPRO" 
 
     #request historical data
-    app.reqHistoricalData(1,contract,'','1 D','1 min','MIDPOINT',0,2,True,[])
+    app.reqHistoricalData(1,contract,'','2 D','3 mins','MIDPOINT',0,2,True,[])
 
     # Call stop() after 3 seconds to disconnect the program
     Timer(1500,app.stop).start()
