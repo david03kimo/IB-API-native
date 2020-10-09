abd0472139a128e89f883003f8f15f466431ee8c
David Liao <david03kimo@gmail.com>
2020-10-10 00:13:08 +0800

    add return after def and small reduce redundant

--- Consolidate3K.py
+++ Consolidate3K.py
@@ -30,13 +30,16 @@ class TestApp(EWrapper,EClient):
             }
         self.now_date=0
         self.pre_date=0
+        return
 
     def error(self,reqId,errorCode,errorString):
         print('Error: ',reqId,' ',errorCode,' ',errorString)
+        return
 
     def historicalData(self,reqId,bar):
         self.data.append([bar.date, bar.open,bar.high,bar.low,bar.close,bar.volume])
         self.now_date=int(bar.date)
+        return
          
     def historicalDataEnd(self, reqId, start: str, end: str):
         self.data1.append(self.data[-1])
@@ -46,41 +49,41 @@ class TestApp(EWrapper,EClient):
         self.df.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/3K.csv',index=0 ,float_format='%.5f')   
         self.data=[] #清掉是否有助於記憶體的節省？
         super().historicalDataEnd(reqId, start, end)
-        print('-------------------------------------------------------------------------------------------------------------')
-        print("HistoricalDataEnd. ReqId:", reqId, "from", start, "to", end)
-        print('-------------------------------------------------------------------------------------------------------------')
-        
+        print( 'HistoricalDataEnd. ReqId:', reqId, 'from', start, 'to', end)
+        return
+
     def historicalDataUpdate(self, reqId: int, bar):
         self.data1.append([bar.date,bar.open,bar.high,bar.low,bar.close,bar.volume])
         self.df1 = pd.DataFrame(self.data1,columns=['DateTime','Open','High','Low', 'Close','Volume'])
         self.df1['DateTime'] = pd.to_datetime(self.df1['DateTime'],unit='s') 
         self.df1=self.df1.set_index('DateTime')
-     
-        #Calculate the bar.date and previous bar.date
-        self.pre_date=self.now_date
+        self.pre_date=self.now_date #Calculate the bar.date and previous bar.date
         self.now_date=int(bar.date)
-        
         if self.now_date != self.pre_date : #Resample once after the bar closed
             res_df=self.df1.resample('3min', closed='left', label='left').agg(self.res_dict)
             del self.data1[0:len(self.data1)-1]
             res_df.drop(res_df.index[-1], axis=0, inplace=True) #delete the new open bar at lastest appended row
-            print('Resampled',datetime.fromtimestamp(self.now_date-180))
             res_df.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/3K.csv', mode='a', header=False,float_format='%.5f')
-        self.df1.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/3K_HistoricalUpdate.csv' ,float_format='%.5f')
-        
+            print('Resampled',datetime.fromtimestamp(self.now_date-180))
+        # self.df1.to_csv('/Users/davidliao/Documents/code/Github/MyProject/data/3K_HistoricalUpdate.csv' ,float_format='%.5f')
+        return
+
     def nextValidId(self,orderId):
         self.nextOrderId=orderId
         self.start()
+        return
 
     def start(self):
         order=Order()
         order.action="SELL"
         order.totalQuantity=90000
         order.orderType="MKT"
+        return
 
     def stop(self):
         self.done=True
         self.disconnect()
+        return
 
 def main():
     app=TestApp()
