839527df9470acbe70c5faa4423a0c79103c6805
David Liao <david03kimo@gmail.com>
2020-12-18 03:43:27 +0800

    add XAU get-data

--- GetHistorical3K_XAUUSD.py
+++ GetHistorical3K_XAUUSD.py
@@ -16,8 +16,8 @@ class TestApp(EWrapper,EClient):
     def __init__(self):
         EClient.__init__(self,self)
         self.data = [] #Initialize variable to store candle
-        self.FX_df={}
-        self.pairs_list=['AUDCAD','AUDCHF','AUDJPY','AUDNZD','AUDUSD','CADCHF','CADJPY','CHFJPY','EURAUD','EURCAD','EURCHF','EURGBP','EURJPY','EURNZD','EURUSD','GBPAUD','GBPCAD','GBPCHF','GBPJPY','GBPNZD','GBPUSD','NZDCAD','NZDCHF','NZDJPY','NZDUSD','USDCAD','USDCHF','USDJPY']
+        self.CMDTY_df={}
+        self.pairs_list=['XAUUSD']
         self.count=0
 
         return
@@ -28,16 +28,16 @@ class TestApp(EWrapper,EClient):
 
     def historicalData(self,reqId,bar):
         # print(f'Time: {bar.date} Open:{bar.open} High:{bar.high} Low:{bar.low} Close: {bar.close} Volume: {bar.volume}')
-        self.FX_df[reqId].append([bar.date, bar.open,bar.high,bar.low,bar.close,bar.volume])
+        self.CMDTY_df[reqId].append([bar.date, bar.open,bar.high,bar.low,bar.close,bar.volume])
         return
 
     def historicalDataEnd(self, reqId, start: str, end: str):
         super().historicalDataEnd(reqId, start, end)
         print( datetime.fromtimestamp(int(datetime.now().timestamp())),'HistoricalDataEnd. ReqId:', reqId, 'from', start, 'to', end)
 
-        self.FX_df[reqId] = pd.DataFrame(self.FX_df[reqId],columns=['DateTime','Open','High','Low', 'Close','Volume'])
-        self.FX_df[reqId]['DateTime'] = pd.to_datetime(self.FX_df[reqId]['DateTime'],unit='s') 
-        self.FX_df[reqId].to_csv('/Users/davidliao/Documents/code/Github/IB-native-API/data/FX_data/'+self.pairs_list[reqId]+'.csv',index=0 ,float_format='%.6f')
+        self.CMDTY_df[reqId] = pd.DataFrame(self.CMDTY_df[reqId],columns=['DateTime','Open','High','Low', 'Close','Volume'])
+        self.CMDTY_df[reqId]['DateTime'] = pd.to_datetime(self.CMDTY_df[reqId]['DateTime'],unit='s') 
+        self.CMDTY_df[reqId].to_csv('/Users/davidliao/Documents/code/Github/IB-native-API/data/CMDTY_data/'+self.pairs_list[reqId]+'.csv',index=0 ,float_format='%.6f')
         self.count+=1
 
         if self.count==len(self.pairs_list):
@@ -53,14 +53,14 @@ class TestApp(EWrapper,EClient):
         self.disconnect()
         return
 
-
-def FX_order(symbol):
-     contract = Contract()
-     contract.symbol = symbol[:3]
-     contract.secType = 'CASH'
-     contract.exchange = 'IDEALPRO'
-     contract.currency = symbol[3:]
-     return contract
+def Commodity(symbol):
+    contract = Contract()
+    contract.symbol = symbol
+    # contract.symbol = symbol[:3]
+    contract.secType = 'CMDTY'
+    contract.exchange = 'SMART'
+    contract.currency = symbol[3:]
+    return contract
 
 
 def main():
@@ -70,8 +70,8 @@ def main():
     app.connect('127.0.0.1',7497,0) # IB TWS
     
     for pair in range(len(app.pairs_list)):
-        contract=FX_order(app.pairs_list[pair])
-        app.FX_df[pair]=[]
+        contract=Commodity(app.pairs_list[pair])
+        app.CMDTY_df[pair]=[]
         print('pairs:',app.pairs_list[pair])
         app.reqHistoricalData(pair,contract,'','1 W','3 mins','MIDPOINT',0,2,False,[]) #request historical data
         
